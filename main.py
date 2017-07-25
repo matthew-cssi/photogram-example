ac4ba9160e466ffb9e9c59237dce788ef4dfe673
Matthew Levine <matthewlevine@google.com>
2017-07-25 11:26:53 -0700

    Added default data for the first run.

--- main.py
+++ main.py
@@ -13,11 +13,30 @@ class Photo(ndb.Model):
     title = ndb.StringProperty()
     photo_url = ndb.StringProperty()
     like_count = ndb.IntegerProperty(default=0)
+    created = ndb.DateTimeProperty(auto_now_add=True)
 
+
+def add_default_photos():
+    # Photo URLs from Wikipedia.
+    cat_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_March_2010-1.jpg/1280px-Cat_March_2010-1.jpg'
+    llama_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Lama_Portrait_06072007_01.jpg/2560px-Lama_Portrait_06072007_01.jpg'
+
+    cat = Photo(title='Cat', photo_url=cat_url, like_count=0)
+    llama = Photo(title='Llama', photo_url=llama_url, like_count=0)
+
+    cat.put()
+    llama.put()
+
+    return [llama, cat]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        photos = Photo.query().fetch()
+        photos = Photo.query().order(-Photo.created).fetch()
+
+        # If there are no photos in the database, add defaults.
+        # This will only happen one time (on the first run).
+        if not photos:
+            photos = add_default_photos()
 
         template_vars = {
             'photos': photos,
